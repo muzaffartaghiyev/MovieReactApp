7ab58f64f878c755e9d8827a2652cdba8f7d2211
muzeffertagiyev <muzaffar.taghiyev@gmail.com>
2025-06-14 14:51:04 +0300

    updated

--- src/auth/firebase.jsx
+++ src/auth/firebase.jsx
@@ -7,13 +7,20 @@ import { getAuth } from "firebase/auth";
 //* https://firebase.google.com/docs/auth/web/start
 //* https://console.firebase.google.com/ => project settings
 //! firebase console settings bölümünden firebaseconfig ayarlarını al
-// TODO: Replace the following with your app's Firebase project configuration
-//!.env dosyası hazırladıktan sonra projeyi durdurup tekrar çalıştırmak lazım
+
+// ! after creating .env app should restarted
+
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
- 
+  apiKey: "AIzaSyA8KlGZbJjn8o365LP31uX6JdibbLCMmuM",
+  authDomain: "moviereactapp-134ee.firebaseapp.com",
+  projectId: "moviereactapp-134ee",
+  storageBucket: "moviereactapp-134ee.firebasestorage.app",
+  messagingSenderId: "1020703556500",
+  appId: "1:1020703556500:web:05663c191e04d7fe940581",
+  measurementId: "G-RJDDTLNSB4"
 };
 
 // Initialize Firebase
